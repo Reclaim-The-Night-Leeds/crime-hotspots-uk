adc4bd5bf532dd44770b07baf983979b544e51f6
George Sykes <el18gs@leeds.ac.uk>
2021-04-10 22:27:23 +0100

    Implemented update constituincy boundaries function

--- src/crime_hotspots_uk/locations/constituincy.py
+++ src/crime_hotspots_uk/locations/constituincy.py
@@ -1,4 +1,5 @@
 import generic
+from crime_hotspots_uk.constants import baseURL, crime_categories_url, constituincies_url, ignore
 
 class constituincy(generic.Locations):
     """
@@ -33,13 +34,36 @@ class constituincy(generic.Locations):
                 multi = self.locations['geometry'][i]
             self.locations.iat[i, 9] = multi
         
-    def update_constituincy_boundaries(self, file_name = 'DEADBEEF'):
+    def update_constituincy_boundaries(self, file_name):
         """ This downloads ne constituincy boundary data from the `ONS GeoPortal <https://geoportal.statistics.gov.uk/datasets/5ce27b980ffb43c39b012c2ebeab92c0_2>`_ This contains the 2018 westminster parkimentary boundaries for the UK.
         
         :param file_name: What to save the downloaded constituincy boundary data as. If nothing is passed it will default to DEADBEEF and take whatever file name is stored in the file_name member variable, defaults to self.file_name
         :type file_name: string, optional
         """
-    
+        
+        # Set the url to get the constituincy data to the URL in constants.py
+        link = constituincies_url
+        
+        # Start streaming the data using python requests
+        resp = requests.get(link, stream=True)
+        
+        # Open the file to save the data to and create a TQDM progress bar to
+        # track the progress (currently the total length is set by the known
+        # size but it should be moved to calculating total file size using
+        # the response headers)
+        with open(file_name, 'wb') as file, tqdm(
+            desc=file_name,
+            total=200517386,
+            unit='iB',
+            unit_scale=True,
+            unit_divisor=1024,
+        ) as bar:
+            for data in resp.iter_content(chunk_size=1024):
+                size = file.write(data)
+                bar.update(size)
+            file.close() # Make sure to close the file after
+        
+        
     def load_constituincy_boundaries(self, file_name = 'DEADBEEF'):
         """ Load in the constituincy boundaries from a specified file.
         
