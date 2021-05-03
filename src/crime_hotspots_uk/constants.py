4247f501caa313cc95e1297e8087a1aaea64bbb1
George Sykes <el18gs@leeds.ac.uk>
2021-05-03 03:56:44 +0100

    Removed depreciated URL for constituinciy boundary URL

--- src/crime_hotspots_uk/constants.py
+++ src/crime_hotspots_uk/constants.py
@@ -2,28 +2,27 @@ baseURL = "https://data.police.uk/api/"
 
 crime_categories_url = "https://data.police.uk/api/crime-categories?date=2020-08"
 
-constituincies_url = 'https://opendata.arcgis.com/datasets/b64677a2afc3466f80d3d683b71c3468_0.geojson'
 
-ignore = ['Sports/Recreation Area', 
-          'Parking Area', 
-          'Park/Open Space', 
-          'Supermarket', 
-          'Petrol Station', 
-          'Pedestrian Subway', 
-          'Shopping Area', 
-          'Further/Higher Education Building', 
-          'Police Station', 
-          'Hospital', 
-          'Nightclub', 
-          'Bus/Coach Station', 
-          'Theatre/Concert Hall', 
-          'Conference/Exhibition Centre', 
-          'Airport/Airfield', 
-          'Added by Police Force', 
-          'Ferry Terminal', 
-          'Theme/Adventure Park', 
-          'Prison', 
-          'Race Track', 
-          'Motorway Service Area']
-
-
+ignore = [
+    "Sports/Recreation Area",
+    "Parking Area",
+    "Park/Open Space",
+    "Supermarket",
+    "Petrol Station",
+    "Pedestrian Subway",
+    "Shopping Area",
+    "Further/Higher Education Building",
+    "Police Station",
+    "Hospital",
+    "Nightclub",
+    "Bus/Coach Station",
+    "Theatre/Concert Hall",
+    "Conference/Exhibition Centre",
+    "Airport/Airfield",
+    "Added by Police Force",
+    "Ferry Terminal",
+    "Theme/Adventure Park",
+    "Prison",
+    "Race Track",
+    "Motorway Service Area",
+]
